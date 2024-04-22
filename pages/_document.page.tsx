3d3b8a1d7b4275b2468604a3c1b77183dd660c4c
John O'Brien <johnobrien8642@gmail.com>
2024-04-22 11:35:04 -0400

    add next third-parties, GA comp

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -3,6 +3,7 @@ import React, { ReactElement } from "react";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 class CustomDocument extends Document {
 	render(): ReactElement {
@@ -14,6 +15,7 @@ class CustomDocument extends Document {
 					<Analytics />
 				</body>
 				<SpeedInsights />
+				<GoogleAnalytics gaId="G-43X5QW7Z85" />
 			</Html>
 		);
 	}
