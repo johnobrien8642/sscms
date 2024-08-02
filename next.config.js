4919cd420b178912a68d17a47a57ff5e5e1b2794
John O'Brien <62157096+johnobrien8642@users.noreply.github.com>
2024-08-01 23:51:19 -0500

    next.config submodules hack

--- next.config.js
+++ next.config.js
@@ -2,32 +2,38 @@ const path = require('path');
 
 module.exports = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "**",
-			},
-			{
-				protocol: "http",
-				hostname: "**",
-			},
-		],
-	},
-	async redirects() {
+	remotePatterns: [
+		{
+			protocol: "https",
+			hostname: "**",
+		},
+		{
+			protocol: "http",
+			hostname: "**",
+		},
+	],
+  },
+  async redirects() {
 		return [
-		  {
-			source: '/models',
-			destination: '/',
-			permanent: true,
-		  },
-		  {
-			source: '/lib',
-			destination: '/',
-			permanent: true,
-		  },
+			{
+				source: '/models',
+				destination: '/',
+				permanent: true,
+		  	},
+		  	{
+				source: '/models/model-types',
+				destination: '/',
+				permanent: true,
+		  	},
+		  	{
+				source: '/lib',
+				destination: '/',
+				permanent: true,
+		  	},
 		]
 	},
 	typescript: {
 		ignoreBuildErrors: true,
 	},
+	pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js']
 };
