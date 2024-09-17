5be38257baf01724941e6427bf0f43d67f19cb53
John O'Brien <62157096+johnobrien8642@users.noreply.github.com>
2024-09-17 06:37:15 -0500

    use draft folderhref if defined

--- core/components/system/Form.tsx
+++ core/components/system/Form.tsx
@@ -242,7 +242,7 @@ const Form = ({
 								pageManagerKey,
 								revalidateAll
 							},
-							folderHref: data[formTitle]?.folderHref
+							folderHref: formCache[formCache?.draftId ?? '']?.folderHref ?? data[formTitle]?.folderHref
 						}),
 						cache: 'no-store'
 					});
